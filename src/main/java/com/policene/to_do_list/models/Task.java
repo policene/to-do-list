fbabea07e2a72135f75ff599f4aebbf3997402d3
Giovanni <giovannicpoli@gmail.com>
2026-03-01 10:26:21 -0300

    initial commit

---
+++ src/main/java/com/policene/to_do_list/models/Task.java
@@ -0,0 +1,14 @@
+package com.policene.to_do_list.models;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Task {
+
+
+
+}
